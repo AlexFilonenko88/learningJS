be7e3770e0f9a22dddf6516110f0b40a98ce0de8
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-08-02 17:28:12 +0300

    task

--- practiceLoopsArray.js
+++ practiceLoopsArray.js
@@ -33,7 +33,7 @@ function Minmax6 (array) {
     return {min, max};
 }
 
-// v;
+// console.log(Minmax6([1,2,3,4,5,6,7,8,9]))
 
 // 3) Series1 Данн массив из десяти вещественных чисел. Найти произведение всех чисел .
 
@@ -52,19 +52,86 @@ function Series1 (arrayNumber) {
 // 4) Minmax1 Дано целое число N и набор из N чисел. Найти минимальный и
 // максимальный из элементов данного набора и вывести их в указанном
 // порядке. (Реализовать поиск в цикле генераци массива)
-//
+
+function Minmax1 (array) {
+    let min = Infinity;
+    let max = -Infinity;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+        if(array[i] > max){
+            max = array[i];
+        }
+    }
+
+    return {min, max};
+}
+
+// console.log(Minmax1([1,2,3,4,5,6,7,8,9,10]))
+
+
 // 5) Series3 Данн массив из N вещественных чисел. Найти их среднее арифметическое.
-//
+
+function Series3 (array) {
+    let w = 0;
+
+    for(let number of array){
+        w += ((number) / array.length);
+    }
+
+    return w;
+}
+
+// console.log(Series3([1,2,3]));
+
 // 6) Series11. Даны целые числа K, N и набор из N целых чисел. Если в наборе
 // имеются числа, меньшие K, то вывести TRUE; в противном случае вывести
 // FALSE.
-//
+
+function Series11 (array, K) {
+    for (let number of array){
+        if(number < K){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// console.log(Series11([1,2,3], 3));
+
 // 7) Series12. Дан набор ненулевых целых чисел; признак его завершения — число 0.
 // Вывести количество чисел в наборе.
-//
+
+// ????????
+function Series12 (array) {
+    return array.length;
+}
+
+// console.log(Series12([1,2,3]));
+
 // 8) Series13. Дан набор ненулевых целых чисел; признак его завершения — число 0.
 // Вывести сумму всех положительных четных чисел из данного набора. Если требуемые числа в наборе отсутствуют, то вывести 0.
-//
+
+function Series13 (array){
+    let sum = 0;
+
+    for(let number of array){
+        if(number < 0 && number % 2 === 1){
+            return false;
+        }
+        if(number > 0 && number % 2 === 0){
+            sum += number;
+        }
+    }
+
+    return sum;
+}
+
+// console.log(Series13([-1,1,3,5,7,9]));
+
 // 9) Array48. Дан целочисленный массив размера N. Найти максимальное количество его одинаковых элементов.
 //
 // 10) Array49. Дан целочисленный массив размера N. Если он является перестановкой, то есть содержит все числа от 1 до N, то вывести 0; в противном
